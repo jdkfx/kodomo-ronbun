82668dc9b1736b9c9272e5513103fb13c466af42
orangefarmer <amateurranch@gmail.com>
2019-07-02 00:51:32 +0000

    追加：ユーザーページ

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\UserDetail;
+use App\Report;
 
 class UsersController extends Controller
 {
     public function show($account_name){
         $user = User::where('account_name',$account_name)->first();
         $user_detail = UserDetail::find($user->id);
+        $reports = Report::where('user_id',$user->id)->get();
 
         return view('users.show',[
             'user' => $user,
             'user_detail' => $user_detail,
+            'reports' => $reports,
         ]);
     }
 }
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,4 +1,47 @@
 @extends('layouts.app')
 @section('content')
-{{ $user->account_name }}
+<div class="userProfile row justify-content-center">
+    <div class="profileImage text-center col-lg-4 col-6 mb-4">
+        <img src="{{ URL::to('/') }}/images/icon.jpg" alt="profileImage" class="img-thumbnail">
+    </div>
+    <div class="profileText col-lg-8">
+        <h4>表示名：{{ $user_detail->display_name }}</h4>
+        <h5>アカウント名：{{ $user->account_name }}</h5>
+        <p>自己紹介：{{ $user_detail->profile_text }}</p>
+    </div>
+</div>
+<div class="reportsIndex">
+    <h3 id="numberOfNavi">投稿したろんぶん 3件</h3>
+    @if(count($reports) > 0)
+        <ul style="padding:0;">
+            @foreach($reports as $report)
+                <li>
+                    <div class="reports card">
+                        <div class="row">
+                            <div class="col-lg-4">
+                                <div class="card-body">
+                                    <?php
+                                    $report_detail = App\ReportDetail::where('report_id',$report->id)->first();
+                                     ?>
+                                    <img src="{{ Storage::url($report_detail->thumbnail) }}" class="img-thumbnail" alt="thumbnailOfReports">
+                                </div>
+                            </div>
+                            <div class="col-lg-8">
+                                <div class="card-body">
+                                    <h3><a href="/reports/{{ $report->id }}">{{ $report->title }}</a></h3>
+                                    <h4><a href="/{{ $user->account_name }}">{!! e($user_detail->display_name) !!}</a></h4>
+                                    <h4>〇年〇月〇日 投稿</h4>
+                                    <?php
+                                    $report_abstract = App\ReportAbstract::where('report_id',$report->id)->first();
+                                     ?>
+                                    <p>{!! nl2br(e($report_abstract->contents_abstract),false) !!}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+</div>
 @endsection
