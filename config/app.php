3d841aefdc1a0497aa0251adba39ed69e16a140b
jdkfx <amateurranch@gmail.com>
2021-02-22 20:54:53 +0900

    更新：Laravelのバージョンを5.* -> 7.30.4

--- config/app.php
+++ config/app.php
@@ -167,7 +167,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Unisharp\Ckeditor\ServiceProvider::class,
 
         /*
          * Application Service Providers...
